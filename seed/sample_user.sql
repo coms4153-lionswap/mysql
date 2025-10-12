428ba92ca55314567262c8a028cd01ae58412d0a
Yuting Liu <candyliu.lyt@gmail.com>
2025-10-12 21:20:00 +0000

    insert sample user data

---
+++ seed/sample_user.sql
@@ -0,0 +1,6 @@
+USE lionswap;
+INSERT INTO Users (uni, student_name, dept_name, email, phone, avatar_url, credibility_score, last_seen_at)
+VALUES
+ ('yl5961', 'Yuting Liu', 'Computer Science', 'yl5961@columbia.edu', NULL, NULL, 5.00, NOW()),
+ ('yg3066', 'Sally Go', 'Computer Science', 'yg3066@columbia.edu', '+1-555-0100', NULL, 4.20, NOW()),
+ ('ab3913', 'Alex Brown', 'Finance', 'ab3913@columbia.edu', NULL, NULL, 3.01, NOW());
